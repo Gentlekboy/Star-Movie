a8c022d83b70a539a42835f14eb79c455cf3be52
gentlekboy <gentlekboy@gmail.com>
2022-02-24 17:25:54 +0100

    initial commit

---
+++ app/src/main/java/com/gentlekboy/starmovie/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gentlekboy.starmovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
