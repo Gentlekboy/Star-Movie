800c9a76e6c6ce32be9448debeb688e3028a2d50
gentlekboy <gentlekboy@gmail.com>
2022-03-08 16:03:47 +0100

    set click events on the login screen

--- app/src/main/java/com/gentlekboy/starmovie/ui/LoginFragment.kt
+++ app/src/main/java/com/gentlekboy/starmovie/ui/LoginFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.gentlekboy.starmovie.R
 import com.gentlekboy.starmovie.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -17,6 +20,38 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setClickEvents()
+    }
+
+    private fun setClickEvents() {
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
+        binding.signUpButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+
+        binding.forgotPassword.setOnClickListener {
+            Snackbar.make(binding.root, "Forgot password screen coming soon", Snackbar.LENGTH_LONG).show()
+        }
+
+        binding.twitterCard.setOnClickListener {
+            Snackbar.make(binding.root, "Twitter login coming soon", Snackbar.LENGTH_LONG).show()
+        }
+
+        binding.facebookCard.setOnClickListener {
+            Snackbar.make(binding.root, "Facebook login coming soon", Snackbar.LENGTH_LONG).show()
+        }
+
+        binding.googlePlusCard.setOnClickListener {
+            Snackbar.make(binding.root, "Google login coming soon", Snackbar.LENGTH_LONG).show()
+        }
     }
 
     override fun onDestroyView() {
