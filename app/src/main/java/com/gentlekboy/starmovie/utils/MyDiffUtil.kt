ee5bf7612b8f9a80851223edb4041f291742aa33
gentlekboy <gentlekboy@gmail.com>
2022-03-08 15:37:13 +0100

    created diff util class for updating data in the recycler view adapter

---
+++ app/src/main/java/com/gentlekboy/starmovie/utils/MyDiffUtil.kt
@@ -0,0 +1,29 @@
+package com.gentlekboy.starmovie.utils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.gentlekboy.starmovie.data.Movie
+
+/**
+ * DiffUtil class for recycler view adapters
+ */
+class MyDiffUtil(
+    private val oldList: ArrayList<Movie>,
+    private val newList: ArrayList<Movie>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize() = oldList.size
+
+    override fun getNewListSize() = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        oldList[oldItemPosition].id == newList[newItemPosition].id
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = when {
+        oldList[oldItemPosition].id != newList[newItemPosition].id -> false
+        oldList[oldItemPosition].rating != newList[newItemPosition].rating -> false
+        oldList[oldItemPosition].title != newList[newItemPosition].title -> false
+        oldList[oldItemPosition].genre != newList[newItemPosition].genre -> false
+        oldList[oldItemPosition].image != newList[newItemPosition].image -> false
+        else -> true
+    }
+}
