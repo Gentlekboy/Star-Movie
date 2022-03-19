1388c2461daeb2d8a553f1a1a18f46a806dc949e
gentlekboy <gentlekboy@gmail.com>
2022-03-20 00:10:33 +0100

    set up view pager and tab layout

--- app/src/main/java/com/gentlekboy/starmovie/ui/MovieDetailFragment.kt
+++ app/src/main/java/com/gentlekboy/starmovie/ui/MovieDetailFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.gentlekboy.starmovie.adapter.MovieViewPagerAdapter
 import com.gentlekboy.starmovie.databinding.FragmentMovieDetailBinding
+import com.google.android.material.tabs.TabLayout
 
 class MovieDetailFragment : Fragment() {
     private var _binding: FragmentMovieDetailBinding? = null
     private val binding get() = _binding!!
+    private lateinit var detailMovieViewPager: MovieViewPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +24,31 @@ class MovieDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setUpViewPager()
+        setUpTabLayout()
+    }
+
+    private fun setUpViewPager() {
+        detailMovieViewPager = MovieViewPagerAdapter(requireActivity().supportFragmentManager)
+
+        detailMovieViewPager.apply {
+            addFragment(DetailFragment(), "Detail")
+            addFragment(ReviewsFragment(), "Reviews")
+            addFragment(ShowTimeFragment(), "Showtime")
+        }
+    }
+
+    private fun setUpTabLayout() {
+        binding.apply {
+            viewPager.adapter = detailMovieViewPager
+            tabLayout.setupWithViewPager(binding.viewPager)
+            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {}
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+        }
     }
 
     override fun onDestroyView() {
