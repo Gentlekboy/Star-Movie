3423d17ecd962e120611c863bf24ddaf78def12d
gentlekboy <gentlekboy@gmail.com>
2022-03-30 15:55:09 +0100

    set up adapters to display dates and times

--- app/src/main/java/com/gentlekboy/starmovie/ui/ShowTimeFragment.kt
+++ app/src/main/java/com/gentlekboy/starmovie/ui/ShowTimeFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.gentlekboy.starmovie.adapter.DateAdapter
+import com.gentlekboy.starmovie.adapter.TimeAdapter
+import com.gentlekboy.starmovie.data.model.dateList
+import com.gentlekboy.starmovie.data.model.timeList
 import com.gentlekboy.starmovie.databinding.FragmentShowTimeBinding
 
 class ShowTimeFragment : Fragment() {
     private var _binding: FragmentShowTimeBinding? = null
     private val binding get() = _binding!!
+    private val timeAdapter: TimeAdapter by lazy { TimeAdapter() }
+    private val dateAdapter: DateAdapter by lazy { DateAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +27,19 @@ class ShowTimeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setUpAdapters()
+    }
+
+    private fun setUpAdapters() {
+        binding.apply {
+            iMaxRecyclerView.adapter = timeAdapter
+            twoDRecyclerView.adapter = timeAdapter
+            dateRecyclerView.adapter = dateAdapter
+        }
+
+        timeAdapter.addTimes(timeList)
+        dateAdapter.addDates(dateList)
     }
 
     override fun onDestroyView() {
