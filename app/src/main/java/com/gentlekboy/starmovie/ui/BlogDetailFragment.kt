64d03ecaf9edbb941902ccd302c811f4e92dac32
gentlekboy <gentlekboy@gmail.com>
2022-04-18 21:37:24 +0100

    set up back navigation on the blog detail screen

--- app/src/main/java/com/gentlekboy/starmovie/ui/BlogDetailFragment.kt
+++ app/src/main/java/com/gentlekboy/starmovie/ui/BlogDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.gentlekboy.starmovie.databinding.FragmentBlogDetailBinding
 
 class BlogDetailFragment : Fragment() {
@@ -21,6 +22,16 @@ class BlogDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setUpClickEvents()
+    }
+
+    private fun setUpClickEvents() {
+        binding.apply {
+            backArrow.setOnClickListener {
+                findNavController().navigateUp()
+            }
+        }
     }
 
     override fun onDestroyView() {
