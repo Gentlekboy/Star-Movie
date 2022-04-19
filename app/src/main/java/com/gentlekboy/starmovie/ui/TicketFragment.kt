53b344526df18d6a73073b40aa3698b3952e10f2
gentlekboy <gentlekboy@gmail.com>
2022-04-19 13:52:35 +0100

    set up adapter for tickets screen

--- app/src/main/java/com/gentlekboy/starmovie/ui/TicketFragment.kt
+++ app/src/main/java/com/gentlekboy/starmovie/ui/TicketFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.gentlekboy.starmovie.R
+import com.gentlekboy.starmovie.adapter.TicketsAdapter
+import com.gentlekboy.starmovie.data.model.ticketsList
 import com.gentlekboy.starmovie.databinding.FragmentTicketBinding
+import com.gentlekboy.starmovie.utils.clickinterface.TicketsClickInterface
 
-class TicketFragment : Fragment() {
+class TicketFragment : Fragment(), TicketsClickInterface {
     private var _binding: FragmentTicketBinding? = null
     private val binding get() = _binding!!
+    private val ticketsAdapter by lazy { TicketsAdapter(this) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,10 +27,27 @@ class TicketFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setUpAdapter()
+    }
+
+    private fun setUpAdapter() {
+        binding.ticketsRecyclerView.adapter = ticketsAdapter
+        ticketsAdapter.addTickets(ticketsList)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun navigateToTicketsDetails(
+        movieTitle: Int,
+        movieName: String,
+        movieShowTime: String,
+        movieShowDate: String,
+        cinemaSeat: String
+    ) {
+        findNavController().navigate(R.id.action_ticketFragment_to_ticketDetailsFragment)
     }
 }
